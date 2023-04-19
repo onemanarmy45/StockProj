8c6b3735012a85987d8d491382926707a204a4a8
Robert J <106358887+onemanarmy45@users.noreply.github.com>
2023-04-19 15:56:26 -0400

    ongoing final proj

--- StockPriceChecker/Controllers/StockPriceController.cs
+++ StockPriceChecker/Controllers/StockPriceController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using StockPriceChecker.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -16,6 +17,25 @@ namespace StockPriceChecker.Controllers
         public async Task<ActionResult> GetStockPrice(string tickerSymbol)
         {
             // Make an API call to retrieve the stock price data using the RealStonks API
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://realstonks.p.rapidapi.com/TSLA"),
+                Headers =
+                {
+                    { "X-RapidAPI-Key", "c0cef5ee96msh2108c722f3fe6dap18231cjsn05d6ed2c81b7" },
+                    { "X-RapidAPI-Host", "realstonks.p.rapidapi.com" },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
+            }
+
+
             using (var httpClient = new HttpClient())
             {
                 var response = await httpClient.GetAsync("https://realstonks.p.rapidapi.com/TSLA" + tickerSymbol);
@@ -25,6 +45,9 @@ namespace StockPriceChecker.Controllers
                     // Parse the JSON response into a StockPrice object
                     var json = await response.Content.ReadAsStringAsync();
                     var stockPrice = JsonConvert.DeserializeObject<StockPrice>(json);
+
+                    ViewBag.Title = ("Stock Price for " + stockPrice.TickerSymbol);
+
 
                     // Return the StockPrice object to the view
                     return View(stockPrice);
